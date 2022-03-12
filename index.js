99650eb4a338688019dea7e489fcd4143ae8a62f
Andrew <andrew@pureaquasprings.com>
2022-03-13 09:40:13 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( birthdayArray, party ) {
+    let thankYouCards = []
+    for ( let i = 0; i < birthdayArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${birthdayArray[i]}, for the wonderful ${party} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+        console.log( startingNumber );
+        startingNumber -= 1;
+      }
+      console.log( startingNumber );
+    }
